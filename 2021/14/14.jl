b9f43b0cb7c6e7233a8f1ca38069c17d50b048c6
Danny Robinson <dannyrobinson@gmx.com>
2021-12-15 00:27:23 +0000

    2021 14 Part 2

--- 2021/14/14.jl
+++ 2021/14/14.jl
@@ -3,7 +3,8 @@ if basename(pwd()) == "aoc"
 end
 using Pkg
 Pkg.activate(".")
-using StatsBase
+using StatsBase: countmap
+using Base.Iterators
 
 function loadfile(filename::AbstractString)
     file = readchomp(filename)
@@ -12,21 +13,53 @@ function loadfile(filename::AbstractString)
     target, rules
 end
 
-function doloop(target, rules)
-    result = ""
-    for (a, b) in zip(target[1:end-1], target[2:end])
-        result *= a
-        result *= rules[a * b]
-    end
-    result *= target[end]
-    result
-end
+"Returns an iterator of each overlapping pair of characters in a string."
+charpairs(str::AbstractString) = (view(str, i-1:i) for i in drop(eachindex(str), 1))
 
 function part1(filename::AbstractString)
     target, rules = loadfile(filename)
-    for _ in 1:10
-        target = doloop(target, rules)
+    for _ = 1:10
+        result = ""
+        for (a, b) in charpairs(target)
+            result *= a
+            result *= rules[a*b]
+        end
+        result *= target[end]
+        target = result
     end
     cm = values(countmap(target))
     maximum(cm) - minimum(cm)
+end
+
+function part2(filename::AbstractString, n::Integer)
+    target, oldrules = loadfile(filename)
+    rules = Dict{String,Tuple{String,String}}(
+        k => (first(k) * v, v * last(k)) for (k, v) in pairs(oldrules)
+    )
+    counts = Dict{String,Int}()
+    for cs in charpairs(target)
+        counts[cs] = get(counts, cs, 0) + 1
+    end
+    for _ = 1:n
+        oldcounts = counts
+        counts = empty(counts)
+        for (k, v) in pairs(oldcounts)
+            if k in keys(rules) && !iszero(v)
+                for o in rules[k]
+                    counts[o] = get(counts, o, 0) + v
+                end
+            end
+        end
+    end
+    charcounts = Dict{Char,Int}((
+        first(target) => 1,
+        last(target) => 1
+    ))
+    for (lr, v) in pairs(counts)
+        l, r = lr
+        charcounts[l] = get(charcounts, l, 0) + v
+        charcounts[r] = get(charcounts, r, 0) + v
+    end
+    map!(v -> v ÷ 2, values(charcounts))
+    maximum(values(charcounts)) - minimum(values(charcounts))
 end
