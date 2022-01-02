8d48ce378619da6002396a983907fdcf3bc64ef6
Danny Robinson <dannyrobinson@gmx.com>
2022-01-02 23:40:39 +0000

    2016 Day 6

--- 2016/6/6.jl
+++ 2016/6/6.jl
@@ -1,4 +1,26 @@
 if basename(pwd()) == "aoc"
     cd("2016/6")
 end
+using StatsBase: countmap
 
+function loadcs(filename)
+    reshape(
+        [c for c in readchomp(filename) if !isspace(c)],
+        length(readline(filename)),
+        :
+    )
+end
+
+function part1(filename::AbstractString)
+    join(findmax(countmap(row))[2] for row in eachrow(loadcs(filename)))
+end
+
+@assert part1("example.txt") == "easter"
+part1("input.txt")
+
+function part2(filename::AbstractString)
+    join(findmin(countmap(row))[2] for row in eachrow(loadcs(filename)))
+end
+
+@assert part2("example.txt") == "advent"
+part2("input.txt")
