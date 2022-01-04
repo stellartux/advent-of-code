20e5e57cbd484e5abb56d55713bcecd21810b9f4
Danny Robinson <dannyrobinson@gmx.com>
2022-01-04 17:30:00 +0000

    2017 Day 4

--- 2017/4/4.jl
+++ 2017/4/4.jl
@@ -2,3 +2,21 @@ if basename(pwd()) == "aoc"
     cd("2017/4")
 end
 
+part1(filename::AbstractString) = count(isvalidpassphrase.(eachline(filename)))
+
+function isvalidpassphrase(line::AbstractString)
+    words = split(line)
+    length(words) == length(unique!(sort!(words)))
+end
+
+@assert part1("example.txt") == 2
+part1("input.txt")
+
+part2(filename::AbstractString) = count(isvalidpassphrase2.(eachline(filename)))
+
+function isvalidpassphrase2(line::AbstractString)
+    words = split(line)
+    length(words) == length(unique!(sort!([join(sort!(split(word, ""))) for word in words])))
+end
+
+part2("input.txt")
