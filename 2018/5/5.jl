fde06d66bfe154cd59c1214ab38f5a959f760292
Danny Robinson <dannyrobinson@gmx.com>
2022-01-05 18:41:27 +0000

    2018 Day 5

--- 2018/5/5.jl
+++ 2018/5/5.jl
@@ -2,3 +2,37 @@ if basename(pwd()) == "aoc"
     cd("2018/5")
 end
 
+part1(filename::AbstractString) = reaction(readchomp(filename))
+
+function reaction(str::AbstractString)
+    len = -1
+    while length(str) != len
+        len = length(str)
+        str = replace(
+            str,
+            Iterators.flatten(
+                (c * C => "", C * c => "")
+                for (c, C) in zip('a':'z', 'A':'Z')
+            )...
+        )
+    end
+    len
+end
+
+@assert reaction("aA") == 0
+@assert reaction("abBA") == 0
+@assert reaction("abAB") == 4
+@assert reaction("aabAAB") == 6
+@assert part1("example.txt") == 10
+part1("input.txt")
+
+function part2(filename::AbstractString)
+    str = readchomp(filename)
+    mini = minimum(uppercase(str))
+    maxi = maximum(lowercase(str))
+    ranges = zip(lowercase(mini):maxi, mini:uppercase(maxi))
+    minimum(reaction, replace(str, c => "", C => "") for (c, C) in ranges)
+end
+
+@assert part2("example.txt") == 4
+part2("input.txt")
