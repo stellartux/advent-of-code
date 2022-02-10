22e13f6815fc8c51cdcd57699741c42ceb08bec1
Danny Robinson <dannyrobinson@gmx.com>
2022-02-10 01:24:39 +0000

    2016 Day 18

--- 2016/18/18.jl
+++ 2016/18/18.jl
@@ -2,3 +2,20 @@ if basename(pwd()) == "aoc"
     cd("2016/18")
 end
 
+function countsafe(str::AbstractString, n::Integer)
+    row = [c == '^' for c in str]
+    next = falses(size(row))
+    total = 0
+    for _ in 1:n
+        total += count(!, row)
+        next[1] = row[2]
+        next[2:end-1] = row[1:end-2] .⊻ row[3:end]
+        next[end] = row[end-1]
+        row, next = next, row
+    end
+    total
+end
+
+@assert countsafe(".^^.^.^^^^", 10) == 38
+@time countsafe(readline("input.txt"), 40)
+@time countsafe(readline("input.txt"), 400000)
