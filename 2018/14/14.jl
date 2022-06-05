fe14d6cfb11f2aacfb14180468236bc8870c2c0b
Danny Robinson <dannyrobinson@gmx.com>
2022-06-05 01:09:12 +0100

    2018 Day 14 Part 1

--- 2018/14/14.jl
+++ 2018/14/14.jl
@@ -2,3 +2,26 @@ if basename(pwd()) == "aoc"
     cd("2018/14")
 end
 
+function part1(len=327901)
+    recipes = [3, 7]
+    function nextindex(i)
+        i += recipes[i] + 1
+        while i > length(recipes)
+            i -= length(recipes)
+        end
+        i
+    end
+    i = 1
+    j = 2
+    while length(recipes) < len + 10
+        append!(recipes, reverse(digits(recipes[i] + recipes[j])))
+        i = nextindex(i)
+        j = nextindex(j)
+    end
+    join(recipes[len+1:len+10])
+end
+
+@assert part1(9) == "5158916779"
+@assert part1(5) == "0124515891"
+
+@time part1()
