855261d128d8711e9eb70ea6fd421945d58a87e8
Danny Robinson <dannyrobinson@gmx.com>
2022-12-26 22:55:29 +0000

    2022 Day 15 Part 2 (cursed)

--- 2022/15/15.jl
+++ 2022/15/15.jl
@@ -34,3 +34,24 @@ end
 @assert partone("example.txt", 10) == 26
 println(partone("input.txt", 2000000))
 
+function parttwo(filename::AbstractString, len::Int)
+    lines = sort!(collect(loadfile(filename)), by=x -> x[2])
+    for y in (len÷4):len
+        b = BitSet(0:len)
+        for (sx, sy, bx, by) in lines
+            d = manhattandistance((sx, sy), (bx, by)) - abs(sy - y)
+            for v in max(0, sx - d):min(len, sx + d)
+                delete!(b, v)
+            end
+        end
+        if !isempty(b)
+            return only(b) * 4000000 + y
+        end
+    end
+end
+
+@assert parttwo("example.txt", 20) == 56000011
+@time println(parttwo("input.txt", 4000000))
+# 11482462818989
+# 21306.558980 seconds (7.28 M allocations: 847.494 GiB, 0.25% gc time)
+# Coincidentally, the length of time it took me to think of the proper way to do it
