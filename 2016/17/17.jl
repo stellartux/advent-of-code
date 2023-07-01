9349524ee8b276908d8119507462e2ca30ac822c
Danny Robinson <dannyrobinson@gmx.com>
2023-07-01 07:31:58 +0100

    2016 Day 17 Julia

--- 2016/17/17.jl
+++ 2016/17/17.jl
@@ -1,4 +1,43 @@
-if basename(pwd()) == "aoc"
-    cd("2016/17")
-end
+using MD5: md5
 
+function day17(seed::AbstractString)
+    stack = ["" => CartesianIndex(1, 1)]
+    shortest = nothing
+    longest = 0
+    while !isempty(stack)
+        value, position = popfirst!(stack)
+        if position == CartesianIndex(4, 4)
+            if isnothing(shortest)
+                shortest = value
+            end
+            longest = length(value)
+        else
+            hash = md5(seed * value)[1:2] = md5(seed * value)[1:2]
+            if hash[1] >> 4 > 10 && position[2] > 1
+                push!(stack, value * 'U' => position - CartesianIndex(0, 1))
+            end
+            if hash[1] & 15 > 10 && position[2] < 4
+                push!(stack, value * 'D' => position + CartesianIndex(0, 1))
+            end
+            if hash[2] >> 4 > 10 && position[1] > 1
+                push!(stack, value * 'L' => position - CartesianIndex(1, 0))
+            end
+            if hash[2] & 15 > 10 && position[1] < 4
+                push!(stack, value * 'R' => position + CartesianIndex(1, 0))
+            end
+            if hash[1] >> 4 > 10 && position[2] > 1
+                push!(stack, value * 'U' => position - CartesianIndex(0, 1))
+            end
+            if hash[1] & 15 > 10 && position[2] < 4
+                push!(stack, value * 'D' => position + CartesianIndex(0, 1))
+            end
+            if hash[2] >> 4 > 10 && position[1] > 1
+                push!(stack, value * 'L' => position - CartesianIndex(1, 0))
+            end
+            if hash[2] & 15 > 10 && position[1] < 4
+                push!(stack, value * 'R' => position + CartesianIndex(1, 0))
+            end
+        end
+    end
+    shortest, longest
+end
