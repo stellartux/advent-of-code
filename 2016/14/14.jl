7e4a4bf7ca5a0e08199cbe8a765aa89cb9d9213e
Danny Robinson <dannyrobinson@gmx.com>
2023-07-01 08:14:49 +0100

    2016 Day 14

--- 2016/14/14.jl
+++ 2016/14/14.jl
@@ -1,4 +1,26 @@
-if basename(pwd()) == "aoc"
-    cd("2016/14")
-end
+using MD5: md5
 
+function day14(salt::AbstractString, extra=0)
+    hashcache = Dict{Int,AbstractString}()
+    hash(n) =
+        get!(hashcache, n) do
+            result = bytes2hex(md5(salt * string(n)))
+            for _ = 1:extra
+                result = bytes2hex(md5(result))
+            end
+            result
+        end
+    found = 0
+    for i = Iterators.countfrom(0)
+        m = match(r"(.)\1\1", hash(i))
+        if !isnothing(m)
+            re = Regex("$(m[1]){5}")
+            if any(n -> occursin(re, hash(n)), i .+ (1:1000))
+                found += 1
+                if found == 64
+                    return i
+                end
+            end
+        end
+    end
+end
