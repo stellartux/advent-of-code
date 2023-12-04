81052a54e216f356516321018acbbd0d75afec73
Danny Robinson <dannyrobinson@gmx.com>
2023-12-04 06:28:03 +0000

    2023 Day 1 refactor

--- 2023/1/1.jl
+++ 2023/1/1.jl
@@ -1,24 +1,35 @@
 #!/usr/bin/env julia
+# usage: julia 1.jl [FILENAME]
 
-println(sum(
-    parse(Int, l[[findfirst(isdigit, l), findlast(isdigit, l)]])
-    for l = eachline(get(ARGS, 1, "input.txt"))))
+module AOC2023Day1
 
-let d = Dict(
-        "zero" => "0",
-        "one" => "1",
-        "two" => "2",
-        "three" => "3",
-        "four" => "4",
-        "five" => "5",
-        "six" => "6",
-        "seven" => "7",
-        "eight" => "8",
-        "nine" => "9")
+partone(lines) =
+    sum(parse(Int, line[[findfirst(isdigit, line), findlast(isdigit, line)]]) for line in lines)
 
-    println(sum(begin
-        m = match(r"^.*?([0-9]|zero|one|two|three|four|five|six|seven|eight|nine)", l)
-        n = match(r"^.*([0-9]|zero|one|two|three|four|five|six|seven|eight|nine)", l)
-        parse(Int, get(d, m[1], m[1]) * get(d, n[1], n[1]))
-    end for l = eachline(get(ARGS, 1, "input.txt"))))
+digits = Dict(
+    "zero" => "0",
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9")
+
+parttwo(lines) =
+    sum(begin
+        first = match(r"[0-9]|zero|one|two|three|four|five|six|seven|eight|nine", line).match
+        last = match(r"^.*([0-9]|zero|one|two|three|four|five|six|seven|eight|nine)", line)[1]
+        parse(Int, get(digits, first, first) * get(digits, last, last))
+    end for line in lines)
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    input = readlines(get(ARGS, 1, joinpath(@__DIR__, "input.txt")))
+    println(partone(input))
+    println(parttwo(input))
 end
+
+end # module
