3def460d56494da1b167c4ec299c15e6a43a7b96
Danny Robinson <dannyrobinson@gmx.com>
2023-12-09 23:15:33 +0000

    2023 Day 8 Part 2

--- 2023/8/8.jl
+++ 2023/8/8.jl
@@ -22,19 +22,35 @@ function partone(leftrights, maps)
     steps = 0
     current = 1
     for lr in Iterators.cycle(leftrights)
-        if current == 26 * 26 * 25 + 26 * 25 + 26
-            break
+        if current == hash("ZZZ")
+            return steps
         end
         current = maps[current, lr]
         steps += 1
     end
-    steps
 end
 
+function parttwo(leftrights, maps)
+    positions = [key for key in 1:size(maps, 1) if !iszero(maps[key, 1]) && isone(key % 26)]
+    cyclestartfound = falses(length(positions))
+    cycleendfound = falses(length(positions))
+    steps = zeros(Int, length(positions))
+    for lr in Iterators.cycle(leftrights)
+        if all(cycleendfound)
+            return lcm(steps)
+        end
+        positions .= maps[positions, lr]
+        isendposition = @. iszero(positions % 26)
+        @. cycleendfound |= cyclestartfound && isendposition
+        @. cyclestartfound |= isendposition
+        @. steps += (cyclestartfound && !cycleendfound)
+    end
+end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     leftrights, maps = load(get(ARGS, 1, joinpath(@__DIR__, "input.txt")))
     println(partone(leftrights, maps))
+    println(parttwo(leftrights, maps))
 end
 
 end # module
