0dc4b2c8f15d6d566c1766e2334671767a155d03
Danny Robinson <dannyrobinson@gmx.com>
2025-05-29 06:06:23 +0100

    2018 Day 22

--- 2018/22/22.jl
+++ 2018/22/22.jl
@@ -1,23 +1,127 @@
-if basename(pwd()) != "22"
-    cd("2018/22")
+#!/usr/bin/env julia
+# usage: julia 22.jl [FILENAME]
+
+module AOC2018Day22
+export Cave, erosionlevel, geologicalindex, risklevel, regiontype, showview
+
+struct Cave
+    depth::Int
+    target::CartesianIndex{2}
+    erosionlevels::Dict{CartesianIndex{2},Int}
+    Cave(depth::Int, target::CartesianIndex{2}) =
+        new(depth, target, Dict(target => 0, CartesianIndex(0, 0) => 0))
 end
 
-depth = 510
-height = 10
-width = 10
+function Cave(s::AbstractString)
+    m = match(r"depth\s*:\s*(\d+)\s*target\s*:\s*(\d+)\s*,\s*(\d+)", s)
+    if isnothing(m)
+        @error "Couldn't parse \"$s\""
+    end
+    Cave(parse(Int, m[1]), CartesianIndex(parse(Int, m[3]), parse(Int, m[2])))
+end
 
-erosionlevel(M, y, x) = (M[y, x] + depth) % 20183
+Base.show(io::IO, cave::Cave) =
+    print(io, "Cave(", cave.depth, ", ", cave.target, ")")
 
-M = zeros(UInt, (height + 1, width + 1))
-M[:, 1] .= range(start=0, step=16807, length=height + 1)
-M[1, :] .= range(start=0, step=48271, length=width + 1)
-for j = 2:height+1
-    for i = 2:width+1
-        M[j, i] = erosionlevel(M, j - 1, i) * erosionlevel(M, j, i - 1)
+function erosionlevel(cave::Cave, coord::CartesianIndex{2})::Int
+    get!(cave.erosionlevels, coord) do
+        mod(geologicalindex(cave, coord) + cave.depth, 20183)
     end
 end
-M[height+1, width+1] = 0
 
-R = UInt8[erosionlevel(M, y + 1, x + 1) % 3 for x = 0:width, y = 0:height]
+const left = CartesianIndex(0, -1)
+const right = CartesianIndex(0, 1)
+const up = CartesianIndex(-1, 0)
+const down = CartesianIndex(1, 0)
 
-Int(sum(R))
+function geologicalindex(cave::Cave, coord::CartesianIndex{2})::Int
+    if coord == cave.target
+        0
+    elseif iszero(coord[1])
+        coord[2] * 16807
+    elseif iszero(coord[2])
+        coord[1] * 48271
+    else
+        erosionlevel(cave, coord + left) * erosionlevel(cave, coord + up)
+    end
+end
+
+risklevel(cave::Cave, coord::CartesianIndex{2}) = mod(erosionlevel(cave, coord), 3)
+risklevel(cave::Cave) = (coord) -> risklevel(cave, coord)
+
+const rocky = '.'
+const wet = '='
+const narrow = '|'
+regiontype(cave::Cave, coord::CartesianIndex{2}) =
+    (rocky, wet, narrow)[1+risklevel(cave, coord)]
+
+showview(cave, range) = showview(stdout, cave, range)
+function showview(io::IO, cave::Cave, range::CartesianIndices{2})
+    for row in eachrow(range)
+        for coord in row
+            print(io, iszero(coord) ? 'M' : coord == cave.target ? 'T' : regiontype(cave, coord))
+        end
+        println(io)
+    end
+end
+
+load = Cave ∘ readchomp
+
+partone(cave::Cave) =
+    sum(risklevel(cave), CartesianIndices((0:cave.target[1], 0:cave.target[2])))
+
+Base.checkbounds(::Type{Bool}, ::Cave, coord::CartesianIndex{2}) = coord[1] >= 0 && coord[2] >= 0
+
+function swaptool(cave::Cave, coord::CartesianIndex{2}, tool::Symbol)
+    type = regiontype(cave, coord)
+    if type == rocky
+        tool == :gear ? :torch : :gear
+    elseif type == wet
+        tool == :gear ? :neither : :gear
+    else
+        tool == :torch ? :neither : :torch
+    end
+end
+
+function istoolallowed(cave::Cave, coord::CartesianIndex{2}, tool::Symbol)
+    type = regiontype(cave, coord)
+    if type == rocky
+        tool == :gear || tool == :torch
+    elseif type == wet
+        tool == :gear || tool == :neither
+    else
+        tool == :torch || tool == :neither
+    end
+end
+
+function parttwo(cave::Cave)
+    q1 = [(0, :torch, CartesianIndex(0, 0))]
+    q2 = empty(q1)
+    visited = ( torch = Set{CartesianIndex{2}}(), gear = Set{CartesianIndex{2}}(), neither = Set{CartesianIndex{2}}() )
+    while true
+        steps, tool, pos = popfirst!(!isempty(q1) && isempty(q2) || q1[1][1] <= q2[1][1] ? q1 : q2)
+        if tool == :torch && pos == cave.target
+            return steps
+        elseif pos ∈ visited[tool]
+            continue
+        end
+        push!(visited[tool], pos)
+        for p in pos .+ (up, down, left, right)
+            if checkbounds(Bool, cave, p) && istoolallowed(cave, p, tool)
+                push!(q1, (steps + 1, tool, p))
+            end
+        end
+        t = swaptool(cave, pos, tool)
+        if istoolallowed(cave, pos, t)
+            push!(q2, (steps + 7, t, pos))
+        end
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    cave = load(get(ARGS, 1, joinpath(@__DIR__, "input.txt")))
+    println(partone(cave))
+    println(parttwo(cave))
+end
+
+end # module
