a809917e91be91c52ae26fe7dc67e63c1970115e
Danny Robinson <dannyrobinson@gmx.com>
2025-06-20 17:20:20 +0100

    Cleanup

--- 2019/intcode.pl
+++ 2019/intcode.pl
@@ -140,3 +140,4 @@ instruction(9, In, In, Out, Out, Base0, Base, IP0, IP) -->
     test(interpret4) :-
         assertion(interpret([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])).
 
+:- end_tests(intcode).
