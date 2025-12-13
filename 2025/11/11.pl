d6f86b060fd9f507d8b5818218101489f51106ee
Danny Robinson <dannyrobinson@gmx.com>
2025-12-13 02:33:30 +0000

    2025 Day 11

--- 2025/11/11.pl
+++ 2025/11/11.pl
@@ -10,30 +10,42 @@
 
 load(Filename, Input) :- once(phrase_from_file(load(Input), Filename)).
 
-load(Result) --> sequence(line, Lines), { foldl(build_graph, Lines, [], Result) }.
-
-build_graph(device(ID, Outputs), Graph0, Graph) :-
-    pairs_keys_values(Edges, IDs, Outputs),
-    maplist(=(ID), IDs),
-    add_edges(Graph0, Edges, Graph).
-
-line(device(ID, Outputs)) --> device(ID), ": ", sequence(device, " ", Outputs), eol.
+load(Graph) --> sequence(line, Graph0), { keysort(Graph0, Graph) }.
+line(ID - Outputs) --> device(ID), ": ", sequence(device, " ", Outputs0), eol, { sort(Outputs0, Outputs) }.
 
 device(ID) --> string_without(": \n", Codes), { atom_codes(ID, Codes) }.
 
-paths(Graph, [Here | Path0], Path) :-
+paths(Graph, Target, [Here | Path0], Path) :-
     Path1 = [There, Here | Path0],
     neighbors(Here, Graph, Theres),
     member(There, Theres),
-    (   There == out
+    (   There == Target
     ->  Path = Path1
-    ;   paths(Graph, Path1, Path)
+    ;   paths(Graph, Target, Path1, Path)
     ).
 
+count_paths(Start, End, Graph0, Result) :-
+    top_sort(Graph0, Sort),
+    once(append(Vs0, [Start | Tail], Sort)),
+    once(append(_, [End | Vs1], Tail)),
+    del_vertices(Graph0, Vs0, Graph1),
+    del_vertices(Graph1, Vs1, Graph),
+    aggregate_all(count, paths(Graph, End, [Start], _), Result).
+
 part_one_load --> load, part_one.
-part_one(Graph, Result) :- aggregate_all(count, paths(Graph, [you], _), Result).
+part_one --> count_paths(you, out).
+
+part_two_load --> load, part_two.
+part_two(Graph, Result) :-
+    count_paths(svr, fft, Graph, Count1),
+    count_paths(fft, dac, Graph, Count2),
+    count_paths(dac, out, Graph, Count3),
+    Result is Count1 * Count2 * Count3.
 
 main([File]) :-
     load(File, Input),
     part_one(Input, ResultOne),
-    writeln(ResultOne).
+    writeln(ResultOne),
+    % 79,411,229,618 inferences, 10944.550 CPU in 10983.536 seconds (100% CPU, 7255779 Lips)
+    time(part_two(Input, ResultTwo)),
+    writeln(ResultTwo).
